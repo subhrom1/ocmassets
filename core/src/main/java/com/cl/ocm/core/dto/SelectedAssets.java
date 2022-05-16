19a0bf256a5169b620e4a1390943c8eab4ba73f3
subhrom1 <shubhro1@gmail.com>
2022-05-16 19:50:59 +0200

    configs

---
+++ core/src/main/java/com/cl/ocm/core/dto/SelectedAssets.java
@@ -0,0 +1,16 @@
+package com.cl.ocm.core.dto;
+
+import java.util.List;
+
+public class SelectedAssets {
+
+    private List<String> links;
+
+    public List<String> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<String> links) {
+        this.links = links;
+    }
+}
